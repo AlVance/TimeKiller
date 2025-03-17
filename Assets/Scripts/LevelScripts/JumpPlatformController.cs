b55e04af2063e28eb6ee9176221cb17877950007
AlVance <alvarotvance@gmail.com>
2025-03-17 12:38:48 +0100

    JumpPlatform corrected velocities and block input time

--- Assets/Scripts/LevelScripts/JumpPlatformController.cs
+++ Assets/Scripts/LevelScripts/JumpPlatformController.cs
@@ -6,14 +6,16 @@ public class JumpPlatformController : MonoBehaviour
     [SerializeField] Transform JumpDirectionTr;
     [SerializeField] float Jumpspeed;
     [SerializeField] ParticleSystem particle;
+    [SerializeField] private float blockInputTime = 0.05f;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             //other.gameObject.GetComponent<Rigidbody>().linearVelocity += Jumpdirection.normalized * Jumpspeed;
             Rigidbody _rb = other.gameObject.GetComponent<Rigidbody>();
-            _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0, _rb.linearVelocity.z);
+            _rb.linearVelocity = new Vector3(0, 0, 0);
             //_rb.AddForce(Jumpdirection.normalized * Jumpspeed);
+            other.gameObject.GetComponent<PlayerController>().BlockPlayer(blockInputTime);
             _rb.AddForce(JumpDirectionTr.up.normalized * Jumpspeed);
 
             particle.Play();
