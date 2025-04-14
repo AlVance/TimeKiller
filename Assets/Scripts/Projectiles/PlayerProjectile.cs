4f9c00c028191bfde860bd8154eb802ee30fd37d
Marywe <kirilopez99@gmail.com>
2025-04-14 18:29:35 +0200

    shoot impact y fixes

--- Assets/Scripts/Projectiles/PlayerProjectile.cs
+++ Assets/Scripts/Projectiles/PlayerProjectile.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerProjectile : Projectile
 {
+    [SerializeField] private GameObject impactParticle;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Enemy" || other.gameObject.layer == LayerMask.NameToLayer("Floor"))
@@ -12,11 +13,19 @@ public class PlayerProjectile : Projectile
                 other.gameObject.GetComponent<EnemyBehaviour>().SetHealth(-projectileDamage);
             }
 
-            if (!launched) 
+            if (!launched)
             {
                 GameManager.Instance.currentPlayer.ResetCharge();
-            } 
-            else Destroy(this.gameObject);
+            }
+            else
+            {
+                Vector3 rotation = this.transform.rotation.eulerAngles;
+                rotation.y -= 180;
+                Instantiate(impactParticle, this.transform.position, Quaternion.Euler(rotation));
+                Destroy(this.gameObject);
+
+            }
+
         }
     }
 }
