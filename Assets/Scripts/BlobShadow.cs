c4adf986d2cd381de022bfaba8dc9b5614a87117
AlVance <alvarotvance@gmail.com>
2025-04-21 12:45:11 +0200

    Fixes, better blob shadow nosequenosecual

--- Assets/Scripts/BlobShadow.cs
+++ Assets/Scripts/BlobShadow.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class BlobShadow : MonoBehaviour
 {
-    [SerializeField] private PlayerController pC;
     [SerializeField] private GameObject shadow;
     [SerializeField] private RaycastHit hit;
     [SerializeField] private float rayOffset;
     [SerializeField] private float hitOffset;
     Vector3 hitPosition;
     [SerializeField] private float rotateSpeed;
+
+    [SerializeField] private Vector2 minMaxSize;
+    [SerializeField] private Vector2 minMaxDistance;
     private void Start()
     {
         shadow.transform.SetParent(null);
@@ -27,6 +29,20 @@ public class BlobShadow : MonoBehaviour
         {
             //print(hit.transform);
             hitPosition = hit.point;
+            SetBlobShadowSize();
+        }
+    }
+
+    private void SetBlobShadowSize()
+    {
+        float distance = Vector3.Distance(this.transform.position, hitPosition);
+        if (distance < minMaxDistance.x) shadow.transform.localScale = new Vector3(minMaxSize.x, 0, minMaxSize.x);
+        else if (distance > minMaxDistance.y) shadow.transform.localScale = new Vector3(minMaxSize.y, 0, minMaxSize.y);
+        else
+        {
+            float cDis = distance / minMaxDistance.y;
+            float scaleValue = minMaxSize.x + (cDis * (minMaxSize.y - minMaxSize.x));
+            shadow.transform.localScale = new Vector3(scaleValue, 0, scaleValue);
         }
     }
 }
