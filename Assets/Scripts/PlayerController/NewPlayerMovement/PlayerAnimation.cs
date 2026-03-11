0456b2f0b2786f2ace1b6a289d712cbb98775c04
AlVance <alvarotvance@gmail.com>
2026-03-11 01:44:53 +0100

    Zobizobibozobi

--- Assets/Scripts/PlayerController/NewPlayerMovement/PlayerAnimation.cs
+++ Assets/Scripts/PlayerController/NewPlayerMovement/PlayerAnimation.cs
@@ -5,45 +5,57 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] private Animator playerAnim;
     private PlayerMovement pMovement;
     private PlayerMovement.MovementStates lastState;
+
+    private PlayerShoot pShoot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         pMovement = GetComponent<PlayerMovement>();
+        pShoot = GetComponent<PlayerShoot>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(lastState == null || pMovement.state != lastState)AnimatePlayer();
+        AnimatePlayer();
     }
 
     private void AnimatePlayer()
     {
-        lastState = pMovement.state;
-        switch (pMovement.state)
+        if (lastState == null || pMovement.state != lastState)
         {
-            
-            case PlayerMovement.MovementStates.Idle:
-                playerAnim.SetTrigger("GoIdle");
-                break;
-            case PlayerMovement.MovementStates.Walking:
-                playerAnim.SetTrigger("GoWalk");
-                break;
-            case PlayerMovement.MovementStates.Air:
-                playerAnim.SetTrigger("GoAirIdle");
-                break;
-            case PlayerMovement.MovementStates.Flying:
-                playerAnim.SetTrigger("GoFly");
-                break;
-            case PlayerMovement.MovementStates.Sliding:
-                playerAnim.SetTrigger("GoSlide");
-                break;
-            case PlayerMovement.MovementStates.SlidingDown:
-                playerAnim.SetTrigger("GoSlide");
-                break;
-            default:
-                playerAnim.SetTrigger("GoIdle");
-                break;
+            lastState = pMovement.state;
+            switch (pMovement.state)
+            {
+
+                case PlayerMovement.MovementStates.Idle:
+                    playerAnim.SetTrigger("GoIdle");
+                    break;
+                case PlayerMovement.MovementStates.Walking:
+                    playerAnim.SetTrigger("GoWalk");
+                    break;
+                case PlayerMovement.MovementStates.Air:
+                    playerAnim.SetTrigger("GoAirIdle");
+                    break;
+                case PlayerMovement.MovementStates.Flying:
+                    playerAnim.SetTrigger("GoFly");
+                    break;
+                case PlayerMovement.MovementStates.Sliding:
+                    playerAnim.SetTrigger("GoSlide");
+                    break;
+                case PlayerMovement.MovementStates.SlidingDown:
+                    playerAnim.SetTrigger("GoSlide");
+                    break;
+                default:
+                    playerAnim.SetTrigger("GoIdle");
+                    break;
+            }
+        }
+
+        playerAnim.SetBool("IsAiming", pMovement.isAiming);
+        if(pShoot.didShoot)
+        {
+            playerAnim.SetTrigger("GoShoot");
         }
     }
 }
--- Assets/Scripts/PlayerController/NewPlayerMovement/PlayerShoot.cs
+++ Assets/Scripts/PlayerController/NewPlayerMovement/PlayerShoot.cs
@@ -1,29 +1,49 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class PlayerShoot : MonoBehaviour
 {
     private PlayerInputs pInputs;
     private PlayerMovement pMovement;
 
+    private bool canAim = true;
+
     [SerializeField] private GameObject playerModel;
+    [SerializeField] private GameObject weaponModel;
+    [SerializeField] private Transform backSocket;
+    [SerializeField] private Transform handSocket;
+
+    [Header("Shoot variables")]
+    [SerializeField] private float shootCDTime;
+    public bool shootOnCD = false;
+    public bool didShoot = false;
 
     private void Start()
     {
         pInputs = GetComponent<PlayerInputs>();
         pMovement = GetComponent<PlayerMovement>();
+
+        weaponModel.transform.parent = backSocket;
+        weaponModel.transform.localPosition = Vector3.zero;
+        weaponModel.transform.localRotation = Quaternion.Euler(Vector3.zero);
     }
 
     private void Update()
     {
-        if (pInputs.shootPressed && !pMovement.isAiming) StartAim();
+        canAim = !pMovement.isSliding;
+
+        if (pInputs.shootPressed && !pMovement.isAiming && canAim) StartAim();
         if(pMovement.isAiming)Aim();
-        if (!pInputs.shootPressed && pMovement.isAiming) EndAim();
+        if (pMovement.isAiming && (!pInputs.shootPressed || !canAim)) EndAim();
     }
 
     private void StartAim()
     {
         pMovement.isAiming = true;
+        weaponModel.transform.parent = handSocket;
+        weaponModel.transform.localPosition = Vector3.zero;
+        weaponModel.transform.localRotation = Quaternion.Euler(Vector3.zero);
     }
 
     private void Aim()
@@ -34,6 +54,31 @@ public class PlayerShoot : MonoBehaviour
 
     private void EndAim()
     {
+        Shoot();
         pMovement.isAiming = false;
+        weaponModel.transform.parent = backSocket;
+        weaponModel.transform.localPosition = Vector3.zero;
+        weaponModel.transform.localRotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    private void Shoot()
+    {
+        if (!shootOnCD)
+        {
+            Debug.Log("SHOOT!");
+            StartCoroutine(_ShootCD());
+        }
+    }
+
+    private IEnumerator _ShootCD()
+    {
+        didShoot = true;
+        yield return null;
+        didShoot = false;
+        shootOnCD = true;
+        weaponModel.SetActive(false);
+        yield return new WaitForSeconds(shootCDTime);
+        weaponModel.SetActive(true);
+        shootOnCD = false;
     }
 }
