17dd61dbc1be77429d39afbd150bb9be66a9cb67
AlVance <alvarotvance@gmail.com>
2026-03-27 16:18:07 +0100

    Camera changes based on speed

--- Assets/Scripts/CamerasFOVController.cs
+++ Assets/Scripts/CamerasFOVController.cs
@@ -5,10 +5,12 @@ public class CamerasFOVController : MonoBehaviour
 {
     [SerializeField] private float initialFOV = 70;
     [SerializeField] private float maxFOV = 100;
-    private float currentFov;
+    [SerializeField] private AnimationCurve FOVCurve;
+    public float currentFov;
     private Rigidbody pRb;
     private List<GameObject> cams = new List<GameObject>();
-
+    [SerializeField] private float changeSpeed;
+    float currentChangeSpeed = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,7 +23,11 @@ public class CamerasFOVController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentFov = pRb.linearVelocity.sqrMagnitude;
+        Debug.Log(pRb.linearVelocity.sqrMagnitude);
+
+        if (currentFov > FOVCurve.Evaluate(pRb.linearVelocity.sqrMagnitude)) currentChangeSpeed = changeSpeed * 4;
+        else currentChangeSpeed = changeSpeed;
+       currentFov = Mathf.Lerp(currentFov, FOVCurve.Evaluate(pRb.linearVelocity.sqrMagnitude), changeSpeed * Time.deltaTime);
         if (currentFov <= initialFOV) currentFov = initialFOV;
         if (currentFov >= maxFOV) currentFov = maxFOV;
 
@@ -33,11 +39,13 @@ public class CamerasFOVController : MonoBehaviour
 
     public void GetLevelCams()
     {
+        if (cams.Count > 0) cams.Clear();
         int camCount = CinemachineCore.VirtualCameraCount;
 
         for (int i = 0; i < camCount; i++)
         {
             cams.Add(CinemachineCore.GetVirtualCamera(i).gameObject);
+            cams[i].GetComponent<CinemachineCamera>().Lens.FieldOfView = initialFOV;
         }
     }
 }
