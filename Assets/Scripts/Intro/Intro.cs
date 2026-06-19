e144143c674826a3b8a4e4d699456d1876e04d5b
Álvaro Torre Van Cutsem <alvarotvance@gmail.com>
2026-06-19 19:16:44 +0200

    Build stuff

--- Assets/Scripts/Intro/Intro.cs
+++ Assets/Scripts/Intro/Intro.cs
@@ -1,69 +1,69 @@
-using UnityEngine;
-using TMPro;
-using System.Collections;
-using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
+//using UnityEngine;
+//using TMPro;
+//using System.Collections;
+//using UnityEditor.SceneManagement;
+//using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
-{
-    //public TextMeshProUGUI textComponent;
-    //public string[] lines;
-    //public float textSpeed;
+//public class NewMonoBehaviourScript : MonoBehaviour
+//{
+//    //public TextMeshProUGUI textComponent;
+//    //public string[] lines;
+//    //public float textSpeed;
 
-    //private int index;
+//    //private int index;
 
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
-    //void Start()
-    //{
-    //    textComponent.text = string.Empty;
-    //    StartCoroutine(IntroEvent());
+//    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+//    //void Start()
+//    //{
+//    //    textComponent.text = string.Empty;
+//    //    StartCoroutine(IntroEvent());
 
-    //}
+//    //}
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if(Input.GetKeyUp(KeyCode.Escape))
-    //    {
-    //        gameObject.SetActive(false);
+//    //// Update is called once per frame
+//    //void Update()
+//    //{
+//    //    if(Input.GetKeyUp(KeyCode.Escape))
+//    //    {
+//    //        gameObject.SetActive(false);
 
-    //        SceneManager.LoadScene("Scenes/GameScenes/BaseScene");
-    //    }
-    //}
-    //IEnumerator IntroEvent()
-    //{
-    //    yield return new WaitForSeconds(4f);
-    //    StartDialogue();
-    //}
-    //void StartDialogue()
-    //{
-    //    index = 0;
-    //    StartCoroutine(TypeLine());
-    //}
-    //IEnumerator TypeLine()
-    //{ 
-    //    foreach (char c in lines[index].ToCharArray())
-    //    {
-    //        textComponent.text += c;
-    //        yield return new WaitForSeconds(textSpeed);
-    //    }
-    //    yield return new WaitForSeconds(2f);
-    //    NextLine();
-    //}
-    //void NextLine()
-    //{
-    //    if (index < lines.Length -1)
-    //    {
-    //        index++;
-    //        textComponent.text = string.Empty;
-    //        StartCoroutine(TypeLine());
-    //    }
-    //    else
-    //    {
-    //        gameObject.SetActive(false);
-    //        new WaitForSeconds(2f);
+//    //        SceneManager.LoadScene("Scenes/GameScenes/BaseScene");
+//    //    }
+//    //}
+//    //IEnumerator IntroEvent()
+//    //{
+//    //    yield return new WaitForSeconds(4f);
+//    //    StartDialogue();
+//    //}
+//    //void StartDialogue()
+//    //{
+//    //    index = 0;
+//    //    StartCoroutine(TypeLine());
+//    //}
+//    //IEnumerator TypeLine()
+//    //{ 
+//    //    foreach (char c in lines[index].ToCharArray())
+//    //    {
+//    //        textComponent.text += c;
+//    //        yield return new WaitForSeconds(textSpeed);
+//    //    }
+//    //    yield return new WaitForSeconds(2f);
+//    //    NextLine();
+//    //}
+//    //void NextLine()
+//    //{
+//    //    if (index < lines.Length -1)
+//    //    {
+//    //        index++;
+//    //        textComponent.text = string.Empty;
+//    //        StartCoroutine(TypeLine());
+//    //    }
+//    //    else
+//    //    {
+//    //        gameObject.SetActive(false);
+//    //        new WaitForSeconds(2f);
 
-    //        SceneManager.LoadScene("Scenes/GameScenes/BaseScene");
-    //    }
-    //}
-}
+//    //        SceneManager.LoadScene("Scenes/GameScenes/BaseScene");
+//    //    }
+//    //}
+//}
